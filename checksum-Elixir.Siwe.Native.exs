efecafdccb5463ea228e83b7ba26c8f82127ca2b
Sonny Scroggin <sonny@scrogg.in>
2023-11-07 14:19:39 -0600

    Add checksum

---
+++ checksum-Elixir.Siwe.Native.exs
@@ -0,0 +1,9 @@
+%{
+  "libsiwe_native-v0.6.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:747aa6e6da37b3c34b4e5d83ffe90cc9e98be2c471ac408dae18b555569e019d",
+  "libsiwe_native-v0.6.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:fff21f2bd5f22474168bffdcc577d47dcdc8894b59cb747cbc3d09144ed64dc0",
+  "libsiwe_native-v0.6.0-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:8fed75d9d3646aa030459e52d9959acdfccd226ed2279e9aaf5144ed1f1139cd",
+  "libsiwe_native-v0.6.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:ab5bab28d02d3a3a8d7a4bf555e9948aee5685d658539a670d75886f8ff5cf1e",
+  "libsiwe_native-v0.6.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:4d6a585a75d9d018fdf4cc3b89e3b4d9621275fb17379037dbdb38bebfc6eb61",
+  "siwe_native-v0.6.0-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:0748d6624381153b1a83b785e0b1fcb1028477d09286efa73e69fd3369f15678",
+  "siwe_native-v0.6.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:2676fb44ff1f76969d2484632d5eb4784f56da083e8089d1a1b41f4f821e0609",
+}
